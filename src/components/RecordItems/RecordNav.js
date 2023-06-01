ba67671d513b6aa28005bac0ce6c05cc7bcf0c1b
IsaacKusi <kusiisaacimpact@yahoo.com>
2023-06-01 16:07:26 +0000

    AppMeta

--- src/components/RecordItems/RecordNav.js
+++ src/components/RecordItems/RecordNav.js
@@ -1,10 +1,11 @@
 
 import { useContext } from "react"
 import DataContext from "../../context/DataContext"
+import { FaMoon, FaSun } from "react-icons/fa"
 
 
 const RecordNav = () => {
-    const {logoutHandler, userAccess} = useContext(DataContext)
+    const { logoutHandler, userAccess } = useContext(DataContext)
 
     return <>
         <main id="record-nav-main">
@@ -12,9 +13,14 @@ const RecordNav = () => {
                 <h1>Sales</h1>
             </div>
             <div className="record-child">
-               {userAccess? <p>Welcome {userAccess.username}</p> : null }
+                {userAccess ? <p>Welcome {userAccess.username}</p> : null}
             </div>
             <div className="record-child">
+                <div className="fa-items">
+                    <FaMoon className="fa-moon" />
+                    <FaSun className="fa-sun" />
+                </div>
+
                 <button onClick={logoutHandler}>Logout</button>
             </div>
         </main>
